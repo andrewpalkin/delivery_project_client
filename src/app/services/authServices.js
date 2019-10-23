0c7b329e103fe56895b68516153716f38657736b
Denis KU <kudenv.work@gmail.com>
2019-10-23 12:21:19 +0300

    authService

--- src/app/services/authServices.js
+++ src/app/services/authServices.js
@@ -7,11 +7,7 @@ const authlogin = async (payload) => {
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({mail,password})
     };
-<<<<<<< HEAD:src/app/services/loginService.js
     const response = await fetch(registerService+'/auth/register', options);
-=======
-    const response = await fetch('/auth/login', options);
->>>>>>> caedcca4464a1094a06450c07ef7dcd8cbef41e4:src/app/services/authServices.js
     handleResponse(response);
     const user = undefined;
     if (user && user.token) {
@@ -24,14 +20,9 @@ const handleResponse= (response) => {
     if (!response.ok) { 
         return Promise.reject(response.statusText);
     }
-<<<<<<< HEAD:src/app/services/loginService.js
 
     return response;
 }
-=======
-    return response.json();
-};
->>>>>>> caedcca4464a1094a06450c07ef7dcd8cbef41e4:src/app/services/authServices.js
 
 async function authLogout() {
     // remove user from local storage to log user out
@@ -40,11 +31,7 @@ async function authLogout() {
         method: 'GET',
         headers: {'Content-Type': 'application/json'}        
     };
-<<<<<<< HEAD:src/app/services/loginService.js
     const response = await fetch(registerService + '/auth/logout', options);
-=======
-    const response = await fetch('/auth/logout', options);
->>>>>>> caedcca4464a1094a06450c07ef7dcd8cbef41e4:src/app/services/authServices.js
     handleResponse(response);
     const user = undefined;
     if (user && user.token) {
@@ -60,16 +47,9 @@ async function authRegister(signupPayload) {
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(signupPayload)
     };
-<<<<<<< HEAD:src/app/services/loginService.js
     const response = await fetch(registerService+'/auth/register', options);
     handleResponse(response);    
     return handleResponse(response);   ;
-=======
-    const response = await fetch('/auth/register', options);
-    handleResponse(response);
-    const info = undefined;   
-    return info;
->>>>>>> caedcca4464a1094a06450c07ef7dcd8cbef41e4:src/app/services/authServices.js
 }
 
 const authServices = {
