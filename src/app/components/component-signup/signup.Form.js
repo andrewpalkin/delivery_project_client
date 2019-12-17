506eb28e91433c50743efb92d875ef033fdcd483
Denis KU <kudenv.work@gmail.com>
2019-12-17 22:45:18 +0200

    Sign Up foem expansion to have gender, gender selector, terms and conditions, user database

--- src/app/components/component-signup/signup.Form.js
+++ src/app/components/component-signup/signup.Form.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { connect } from 'react-redux'
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 import {Button, Form, Grid, Header, Message, Segment} from "semantic-ui-react";
 import {requiredCommonValdation} from "../../utils/FormFieldValidator";
 
-import {Field, reduxForm} from "redux-form";
+import {Field, reduxForm, formValueSelector } from "redux-form";
 
 import {default as renderInput} from "../../renders/renderInput";
 
@@ -13,13 +14,23 @@ const renderRadio = field => (
         checked={field.input.value === field.radioValue}
         label={field.label}
         name={field.input.name}
-        onChange={(e, {checked}) => field.input.onChange(field.radioValue)}
+        onChange={() => field.input.onChange(field.radioValue)}
     />
 );
 
-const SignupForm = props => {
-    const {handleSubmit, pristine, submitting, signup} = props;
+const renderSelect = (field,{...props}) => (
+    <Form.Select
+      label={field.label}
+      name={field.input.name}
+      onChange={(e, { value }) => field.input.onChange(value)}
+      options={field.options}
+      placeholder={field.placeholder}
+      value={field.input.value}
+    />
+  );
 
+let SignupForm = props => {
+    const {handleSubmit, pristine, submitting, signup, isGenderFree} = props;     
     return (
         <Grid.Column style={{maxWidth: 480}}>
             <Header as="h2" color="teal" textAlign="center">
@@ -72,25 +83,49 @@ const SignupForm = props => {
                         <Field
                             component={renderRadio}
                             label="Female"
-                            name="female"
+                            name="radioGender"
                             value='1'
-                            radioValue={1}
+                            radioValue={1}                            
                         />
                         <Field
                             component={renderRadio}
                             label="Male"
-                            name="Male"
+                            name="radioGender"
                             value='2'
                             radioValue={2}
                         />
                         <Field
                             component={renderRadio}
                             label="Custom"
-                            name="Custom"
+                            name="radioGender"
                             value='3'
                             radioValue={3}
                         />
-                    </Form.Group>
+                        </Form.Group>   
+                        {
+                            isGenderFree === 3 && (
+                                <Form.Group>
+                                    <Field
+                                        component={renderSelect}                            
+                                        name="gender"
+                                        options={[
+                                            { key: "m", text: "She", value: "she" },
+                                            { key: "f", text: "He", value: "He" },
+                                            { key: "t", text: "They", value: "They" }
+                                        ]}
+                                        placeholder="Gender"
+                                    />
+                                </Form.Group>           
+                            )
+                        }                         
+                             <Message 
+                                compact
+                                info={true}
+                                icon='inbox'
+                                header='Policy Aggrements'
+                                content='By clicking Sign Up you agree to our Terms, Data Policy, and Cookies Policy, you may receive Notification from us and can opt out any time' 
+                             />  
+                              <Form.Checkbox label='I agree to the Terms and Conditions' />                                                                                                                               
                     <Button
                         color="teal"
                         fluid
@@ -118,7 +153,20 @@ SignupForm.propTypes = {
     submitting: PropTypes.bool
 };
 
-export default reduxForm({
+SignupForm = reduxForm({
     form: "signupForm",
     validate: requiredCommonValdation
 })(SignupForm);
+
+const selector = formValueSelector('signupForm') // <-- same as form name
+
+SignupForm = connect(
+    state => {
+        const isGenderFree = selector(state, 'radioGender');
+        return {
+            isGenderFree
+        }
+    }
+)(SignupForm)
+
+export default SignupForm;
